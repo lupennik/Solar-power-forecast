bf95c6f2d5213eee81beb70461eae1a664e52449
Lupenko <lupennik@cvut.cz>
2018-03-29 14:04:09 +0200

    Problems with SVM fixed

--- data_receiver.py
+++ data_receiver.py
@@ -1,16 +1,7 @@
 import re
 from urllib.request import *
 import json
-
-# inserted explicitly
-parameters = ["fog", "hail", "heatingdegreedays", "humidity", "maxdewpti", "maxdewptm", "maxhumidity",
-              "maxpressurei",
-              "maxpressurem", "maxtempi", "maxtempm", "maxvisi", "maxvism", "maxwspdi", "maxwspdm", "meandewpti",
-              "meandewptm", "meanpressurei", "meanpressurem", "meantempi", "meantempm", "meanvisi", "meanvism",
-              "meanwdird",
-              "meanwindspdi", "meanwindspdm", "mindewpti", "mindewptm", "minhumidity", "minpressurei",
-              "minpressurem",
-              "mintempi", "mintempm", "minvisi", "minvism", "minwspdi", "minwspdm", "rain", "snow"]
+import datetime
 
 def read_weather_from_file():
     fr = open("weather", 'r')
@@ -25,24 +16,6 @@ def read_astronomic_from_file():
     fr.close()
     parsed_json = json.loads(json_string)
     return parsed_json
-
-
-def read_w_history_from_file(params, n_days):
-    fr = open("history", 'r')
-    json_string = fr.read()
-    fr.close()
-    parsed_json = json.loads(json_string)
-
-    statistic_data = [[] for i in range(n_days)]
-
-    for i in range(n_days):
-        for item in params:
-            statistic_data[params.index(item)].append(parsed_json[i]["history"]["dailysummary"][0][item])
-
-
-    print(statistic_data)
-    return statistic_data
-
 
 def write_weather_to_file(API_key, country_shortcut, city_name):
     f = urlopen('http://api.wunderground.com/api/' + API_key + '/hourly10day/q/' + country_shortcut + '/' + city_name + '.json')
@@ -81,29 +54,52 @@ def write_sun_angles_to_file(forecast_date, city_name, lat, lat_min, lon, lon_mi
     fp.close()
     return texted_angles
 
-def write_w_history_to_file(forecast_date, n_days, API_key, country_shortcut, city_name, params):
-    f_d = forecast_date
-    parsed_json = {}
+# inserted explicitly
+parameters = ["fog", "hail", "heatingdegreedays", "humidity", "maxdewpti", "maxdewptm", "maxhumidity",
+              "maxpressurei",
+              "maxpressurem", "maxtempi", "maxtempm", "maxvisi", "maxvism", "maxwspdi", "maxwspdm", "meandewpti",
+              "meandewptm", "meanpressurei", "meanpressurem", "meantempi", "meantempm", "meanvisi", "meanvism",
+              "meanwdird",
+              "meanwindspdi", "meanwindspdm", "mindewpti", "mindewptm", "minhumidity", "minpressurei",
+              "minpressurem",
+              "mintempi", "mintempm", "minvisi", "minvism", "minwspdi", "minwspdm", "rain", "snow"]
 
-    statistic_data = [[] for i in range(len(params))]
-    #statistic_data = [[] for i in range(n_days)]
+#writes history weather using API from forecast date to (forecast date - n_days)
+def write_w_history_to_file(forecast_date, n_days, API_key, country_shortcut, city_name, params):
+    date = datetime.datetime(int(forecast_date[:4]), int(forecast_date[5:7]), int(forecast_date[8:10]))
+    parsed_json = {}
+    statistic_data = [[] for i in range(n_days)]
 
     for i in range(n_days):
-        f = urlopen('http://api.wunderground.com/api/' + API_key + '/' + 'history_' + f_d[:4] + f_d[5:7] + f_d[8:10] + '/q' + '/' + country_shortcut + '/' + city_name + '.json')
+        d_iso = date.isoformat()
+        f = urlopen('http://api.wunderground.com/api/' + API_key + '/' + 'history_' + d_iso[:4] + d_iso[5:7] + d_iso[8:10] + '/q' + '/' + country_shortcut + '/' + city_name + '.json')
         json_string = f.read()
         f.close()
-        parsed_json[f_d] = json.loads(json_string)
-        fw = open("history", 'w')
-        fw.write(json.dumps(parsed_json, indent=4, sort_keys=True))
-        fw.close()
+        parsed_json[d_iso[:10]] = json.loads(json_string)
+
+        with open("history", 'w') as fw:
+            fw.write(json.dumps(parsed_json, indent=4, sort_keys=True))
 
         for item in params:
-            print(item + ': ' + parsed_json[f_d]["history"]["dailysummary"][0][item])
-            statistic_data[params.index(item)].append(parsed_json[f_d]["history"]["dailysummary"][0][item])
-            #statistic_data[i].append(parsed_json[f_d]["history"]["dailysummary"][0][item])
+            statistic_data[i].append(parsed_json[d_iso[:10]]["history"]["dailysummary"][0][item])
 
+        date -= datetime.timedelta(days=1)
 
-        i = i + 1
+    return statistic_data
+
+def read_w_history_from_file(forecast_date, n_days, params):
+    f_d = forecast_date
+    fr = open("history", 'r')
+    json_string = fr.read()
+    fr.close()
+    parsed_json = json.loads(json_string)
+
+    statistic_data = [[] for i in range(n_days)]
+
+    for i in range(n_days):
+        for item in params:
+            statistic_data[i].append(parsed_json[f_d]["history"]["dailysummary"][0][item])
+
         f_d = f_d[:8] +  str(int(f_d[8:10]) - 1)
 
-    return params, statistic_data
+    return statistic_data
--- svm_main.py
+++ svm_main.py
@@ -9,32 +9,40 @@ country_shortcut = 'CZ'
 API_key = 'f70b0eafc160a74b'
 forecast_date = '2018-02-13'
 
+"""
 # Generate sample data
-par, X = write_w_history_to_file(forecast_date, 5, API_key, country_shortcut, city_name, parameters)
+#X = write_w_history_to_file(forecast_date, 5, API_key, country_shortcut, city_name, parameters)
+X = read_w_history_from_file(forecast_date, 5, parameters)
 # Read real power load data
 y = [11.95, 8.75, 2.875, 3.275, 7.9 ] #09, 10, 11, 12, 13...  17.825 - 14.02.
-
 # #############################################################################
 # Fit regression model
 par, forecast_day_weather = write_w_history_to_file(forecast_date[:8] + str(int(forecast_date[8:]) + 1), 1, API_key, country_shortcut, city_name, parameters)
-
-
 svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
 svr_lin = SVR(kernel='linear', C=1e3)
 svr_poly = SVR(kernel='poly', C=1e3, degree=2)
-y_rbf = svr_rbf.fit(X, y).predict(forecast_day_weather)
-y_lin = svr_lin.fit(X, y).predict(forecast_day_weather)
-y_poly = svr_poly.fit(X, y).predict(forecast_day_weather)
+y_rbf = svr_rbf.fit(X, y)
+y_lin = svr_lin.fit(X, y)
+y_poly = svr_poly.fit(X, y)
+
+X.append(forecast_day_weather[0])
+y.append(17.825)
+
+y_rbf = svr_rbf.predict(X)
+y_lin = svr_lin.predict(X)
+y_poly = svr_poly.predict(X)
 
 # #############################################################################
 # Look at the results
+x = [1, 2, 3, 4, 5, 6]
 lw = 2
-plt.scatter(X, y, color='darkorange', label='data')
-plt.plot(X, y_rbf, color='navy', lw=lw, label='RBF model')
-plt.plot(X, y_lin, color='c', lw=lw, label='Linear model')
-plt.plot(X, y_poly, color='cornflowerblue', lw=lw, label='Polynomial model')
+plt.plot(x, y, color='navy', lw=lw, label='RBF model')
+plt.plot(x, y_rbf, color='navy', lw=lw, label='RBF model')
+plt.plot(x, y_lin, color='c', lw=lw, label='Linear model')
+plt.plot(x, y_poly, color='cornflowerblue', lw=lw, label='Polynomial model')
 plt.xlabel('data')
 plt.ylabel('target')
 plt.title('Support Vector Regression')
 plt.legend()
 plt.show()
+"""
